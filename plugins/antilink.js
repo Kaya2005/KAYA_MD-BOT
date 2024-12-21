9e0f7925d48adae74c5a4835db42b29c72031ee1
Kaya2005 <harveykaya1@gmail.com>
2024-12-21 14:21:48 +0200

    kaya md

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -1,1 +1,136 @@
-const _0xa1f64d=_0x22dd;function _0x22dd(_0x38dd40,_0x289d21){const _0x1ca580=_0x1ca5();return _0x22dd=function(_0x22dd64,_0x4e64a6){_0x22dd64=_0x22dd64-0x1ef;let _0x560442=_0x1ca580[_0x22dd64];return _0x560442;},_0x22dd(_0x38dd40,_0x289d21);}(function(_0x292c2f,_0x332c3d){const _0x1acb8d=_0x22dd,_0x1f2aef=_0x292c2f();while(!![]){try{const _0x5048c2=parseInt(_0x1acb8d(0x1f4))/0x1+parseInt(_0x1acb8d(0x200))/0x2*(-parseInt(_0x1acb8d(0x1f3))/0x3)+parseInt(_0x1acb8d(0x1fd))/0x4*(-parseInt(_0x1acb8d(0x208))/0x5)+-parseInt(_0x1acb8d(0x1ef))/0x6+-parseInt(_0x1acb8d(0x20d))/0x7+parseInt(_0x1acb8d(0x20e))/0x8*(parseInt(_0x1acb8d(0x202))/0x9)+parseInt(_0x1acb8d(0x1f9))/0xa;if(_0x5048c2===_0x332c3d)break;else _0x1f2aef['push'](_0x1f2aef['shift']());}catch(_0x3b187d){_0x1f2aef['push'](_0x1f2aef['shift']());}}}(_0x1ca5,0xe988b));const {cmd,commands}=require(_0xa1f64d(0x20c)),{readEnv}=require('../lib/database'),fs=require('fs'),path=require(_0xa1f64d(0x1f5));cmd({'on':'body'},async(_0x5ed78f,_0x832e95,_0x45ec53,{from:_0x4b7706,body:_0x3722db,isGroup:_0x36dd2d,isAdmins:_0x5568f7,isBotAdmins:_0x582132,reply:_0x3e9547,sender:_0x3685aa})=>{const _0x871eba=_0xa1f64d;try{const _0x17e332=['wtf',_0x871eba(0x1f2),_0x871eba(0x1fa),'fuck',_0x871eba(0x1f8),'huththa',_0x871eba(0x1fc),_0x871eba(0x1f6),'hutto','lol'];if(!_0x36dd2d||_0x5568f7||!_0x582132)return;const _0x11ca71=_0x3722db[_0x871eba(0x1fb)](),_0x96e591=_0x17e332[_0x871eba(0x20b)](_0x57afcd=>_0x11ca71[_0x871eba(0x1f7)](_0x57afcd)),_0x28c876=await readEnv();_0x96e591&&_0x28c876['ANTI_BAD']===_0x871eba(0x209)&&(await _0x5ed78f[_0x871eba(0x1f1)](_0x4b7706,{'delete':_0x832e95[_0x871eba(0x206)]},{'quoted':_0x832e95}),await _0x5ed78f[_0x871eba(0x1f1)](_0x4b7706,{'text':_0x871eba(0x1ff),'contextInfo':{'mentionedJid':[_0x871eba(0x20a)],'groupMentions':[],'forwardingScore':0x1,'isForwarded':!![],'forwardedNewsletterMessageInfo':{'newsletterJid':_0x871eba(0x204),'newsletterName':_0x871eba(0x1fe),'serverMessageId':0x3e7},'externalAdReply':{'title':_0x871eba(0x1f0),'body':'ꜱᴀᴅᴇᴇꜱʜᴀ\x20ᴛʜᴀʀᴜᴍɪɴ','mediaType':0x1,'sourceUrl':'https://github.com/sadiyamin','thumbnailUrl':'https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/20241214_204755.jpg','renderLargerThumbnail':![],'showAdAttribution':!![]}}},{'quoted':_0x832e95}));}catch(_0x2fddce){console['error'](_0x2fddce),_0x3e9547(_0x871eba(0x210));}});const linkPatterns=[/https?:\/\/(?:chat\.whatsapp\.com|wa\.me)\/\S+/gi,/^https?:\/\/(www\.)?whatsapp\.com\/channel\/([a-zA-Z0-9_-]+)$/];function _0x1ca5(){const _0x37ad34=['1878057fTteei','ANTI_LINK','120363192254044294@newsletter','body','key','error','5wdZekf','true','94779062397@s.whatsapp.net','some','../command','6279000IqokgO','40NGpVon','ꜱᴀᴅᴇᴇꜱʜᴀ\x20ᴛʜᴀʀᴜᴍɪɴ','An\x20error\x20occurred\x20while\x20processing\x20the\x20message.','2571324OeRLlz','LARA\x20MD','sendMessage','mia','2313SJLkgZ','1894417nUJDJW','path','ponnaya','includes','sex','10770250IWxTIe','xxx','toLowerCase','pakaya','2196852iKgdPR','Lααɾα-ᴍᴅ\x20✻','*🚫\x20⚠️BAD\x20WORDS\x20NOT\x20ALLOWED⚠️\x20🚫*','3070XPkXBm','https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/20241214_204755.jpg'];_0x1ca5=function(){return _0x37ad34;};return _0x1ca5();}cmd({'on':_0xa1f64d(0x205)},async(_0x2a18f7,_0x51bfd4,_0x102fb5,{from:_0x158597,body:_0xe034a9,sender:_0x40cc0b,isGroup:_0x546f63,isAdmins:_0x5ae765,isBotAdmins:_0x11ea3c,reply:_0x4be882})=>{const _0x26c06f=_0xa1f64d;try{if(!_0x546f63||_0x5ae765||!_0x11ea3c)return;const _0x5d54b9=linkPatterns[_0x26c06f(0x20b)](_0x2d8e8b=>_0x2d8e8b['test'](_0xe034a9)),_0xd7b6a2=await readEnv();_0x5d54b9&&_0xd7b6a2[_0x26c06f(0x203)]==='true'&&(await _0x2a18f7[_0x26c06f(0x1f1)](_0x158597,{'delete':_0x51bfd4['key']},{'quoted':_0x51bfd4}),await _0x2a18f7['sendMessage'](_0x158597,{'text':'*⚠️\x20Links\x20are\x20not\x20allowed\x20in\x20this\x20group*\x0a@'+_0x40cc0b['split']('@')[0x0]+'\x20has\x20been\x20removed.\x20🚫','mentions':[_0x40cc0b],'contextInfo':{'mentionedJid':[_0x26c06f(0x20a)],'groupMentions':[],'forwardingScore':0x1,'isForwarded':!![],'forwardedNewsletterMessageInfo':{'newsletterJid':_0x26c06f(0x204),'newsletterName':_0x26c06f(0x1fe),'serverMessageId':0x3e7},'externalAdReply':{'title':_0x26c06f(0x1f0),'body':_0x26c06f(0x20f),'mediaType':0x1,'sourceUrl':'https://github.com/sadiyamin','thumbnailUrl':_0x26c06f(0x201),'renderLargerThumbnail':![],'showAdAttribution':!![]}}},{'quoted':_0x51bfd4}));}catch(_0x22a8c7){console[_0x26c06f(0x207)](_0x22a8c7),_0x4be882(_0x26c06f(0x210));}});
+const _0xa1f64d = _0x22dd;
+function _0x22dd(_0x38dd40, _0x289d21) {
+    const _0x1ca580 = _0x1ca5();
+    return _0x22dd = function (_0x22dd64, _0x4e64a6) {
+        _0x22dd64 = _0x22dd64 - 0x1ef;
+        let _0x560442 = _0x1ca580[_0x22dd64];
+        return _0x560442;
+    }, _0x22dd(_0x38dd40, _0x289d21);
+}(function (_0x292c2f, _0x332c3d) {
+    const _0x1acb8d = _0x22dd,
+        _0x1f2aef = _0x292c2f();
+    while (!![]) {
+        try {
+            const _0x5048c2 = parseInt(_0x1acb8d(0x1f4)) / 0x1 + parseInt(_0x1acb8d(0x200)) / 0x2 * (-parseInt(_0x1acb8d(0x1f3)) / 0x3) + parseInt(_0x1acb8d(0x1fd)) / 0x4 * (-parseInt(_0x1acb8d(0x208)) / 0x5) + -parseInt(_0x1acb8d(0x1ef)) / 0x6 + -parseInt(_0x1acb8d(0x20d)) / 0x7 + parseInt(_0x1acb8d(0x20e)) / 0x8 * (parseInt(_0x1acb8d(0x202)) / 0x9) + parseInt(_0x1acb8d(0x1f9)) / 0xa;
+            if (_0x5048c2 === _0x332c3d) break;
+            else _0x1f2aef['push'](_0x1f2aef['shift']());
+        } catch (_0x3b187d) {
+            _0x1f2aef['push'](_0x1f2aef['shift']());
+        }
+    }
+}(_0x1ca5, 0xe988b));
+const {
+    cmd,
+    commands
+} = require(_0xa1f64d(0x20c)), {
+    readEnv
+} = require('../lib/database'), fs = require('fs'), path = require(_0xa1f64d(0x1f5));
+cmd({
+    'on': 'body'
+}, async (_0x5ed78f, _0x832e95, _0x45ec53, {
+    from: _0x4b7706,
+    body: _0x3722db,
+    isGroup: _0x36dd2d,
+    isAdmins: _0x5568f7,
+    isBotAdmins: _0x582132,
+    reply: _0x3e9547,
+    sender: _0x3685aa
+}) => {
+    const _0x871eba = _0xa1f64d;
+    try {
+        const _0x17e332 = ['wtf', _0x871eba(0x1f2), _0x871eba(0x1fa), 'fuck', _0x871eba(0x1f8), 'huththa', _0x871eba(0x1fc), _0x871eba(0x1f6), 'hutto', 'lol'];
+        if (!_0x36dd2d || _0x5568f7 || !_0x582132) return;
+        const _0x11ca71 = _0x3722db[_0x871eba(0x1fb)](),
+            _0x96e591 = _0x17e332[_0x871eba(0x20b)](_0x57afcd => _0x11ca71[_0x871eba(0x1f7)](_0x57afcd)),
+            _0x28c876 = await readEnv();
+        _0x96e591 && _0x28c876['ANTI_BAD'] === _0x871eba(0x209) && (await _0x5ed78f[_0x871eba(0x1f1)](_0x4b7706, {
+            'delete': _0x832e95[_0x871eba(0x206)]
+        }, {
+            'quoted': _0x832e95
+        }), await _0x5ed78f[_0x871eba(0x1f1)](_0x4b7706, {
+            'text': _0x871eba(0x1ff),
+            'contextInfo': {
+                'mentionedJid': [_0x871eba(0x20a)],
+                'groupMentions': [],
+                'forwardingScore': 0x1,
+                'isForwarded': !![],
+                'forwardedNewsletterMessageInfo': {
+                    'newsletterJid': _0x871eba(0x204),
+                    'newsletterName': _0x871eba(0x1fe),
+                    'serverMessageId': 0x3e7
+                },
+                'externalAdReply': {
+                    'title': _0x871eba(0x1f0),
+                    'body': 'ꜱᴀᴅᴇᴇꜱʜᴀ ᴛʜᴀʀᴜᴍɪɴ',
+                    'mediaType': 0x1,
+                    'sourceUrl': 'https://github.com/sadiyamin',
+                    'thumbnailUrl': 'https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/20241214_204755.jpg',
+                    'renderLargerThumbnail': ![],
+                    'showAdAttribution': !![]
+                }
+            }
+        }, {
+            'quoted': _0x832e95
+        }));
+    } catch (_0x2fddce) {
+        console['error'](_0x2fddce), _0x3e9547(_0x871eba(0x210));
+    }
+});
+const linkPatterns = [/https?:\/\/(?:chat\.whatsapp\.com|wa\.me)\/\S+/gi, /^https?:\/\/(www\.)?whatsapp\.com\/channel\/([a-zA-Z0-9_-]+)$/];
+function _0x1ca5() {
+    const _0x37ad34 = ['1878057fTteei', 'ANTI_LINK', '120363192254044294@newsletter', 'body', 'key', 'error', '5wdZekf', 'true', '243823661981@s.whatsapp.net', 'some', '../command', '6279000IqokgO', '40NGpVon', '𝑯𝒆𝒓𝒗𝒆 𝒌𝒂𝒚𝒂', 'An error occurred while processing the message.', '2571324OeRLlz', 'KAYA MD', 'sendMessage', 'mia', '2313SJLkgZ', '1894417nUJDJW', 'path', 'ponnaya', 'includes', 'sex', '10770250IWxTIe', 'xxx', 'toLowerCase', 'pakaya', '2196852iKgdPR', '𝙆𝘼𝙔𝘼♱𝙈𝘿', '*🚫 ⚠️BAD WORDS NOT ALLOWED⚠️ 🚫*', '3070XPkXBm', 'https://raw.githubusercontent.com/tharumin/Alexa_Voice/refs/heads/main/20241214_204755.jpg'];
+    _0x1ca5 = function () {
+        return _0x37ad34;
+    };
+    return _0x1ca5();
+}
+cmd({
+    'on': _0xa1f64d(0x205)
+}, async (_0x2a18f7, _0x51bfd4, _0x102fb5, {
+    from: _0x158597,
+    body: _0xe034a9,
+    sender: _0x40cc0b,
+    isGroup: _0x546f63,
+    isAdmins: _0x5ae765,
+    isBotAdmins: _0x11ea3c,
+    reply: _0x4be882
+}) => {
+    const _0x26c06f = _0xa1f64d;
+    try {
+        if (!_0x546f63 || _0x5ae765 || !_0x11ea3c) return;
+        const _0x5d54b9 = linkPatterns[_0x26c06f(0x20b)](_0x2d8e8b => _0x2d8e8b['test'](_0xe034a9)),
+            _0xd7b6a2 = await readEnv();
+        _0x5d54b9 && _0xd7b6a2[_0x26c06f(0x203)] === 'true' && (await _0x2a18f7[_0x26c06f(0x1f1)](_0x158597, {
+            'delete': _0x51bfd4['key']
+        }, {
+            'quoted': _0x51bfd4
+        }), await _0x2a18f7['sendMessage'](_0x158597, {
+            'text': '*⚠️ Links are not allowed in this group*\x0a@' + _0x40cc0b['split']('@')[0x0] + ' has been removed. 🚫',
+            'mentions': [_0x40cc0b],
+            'contextInfo': {
+                'mentionedJid': [_0x26c06f(0x20a)],
+                'groupMentions': [],
+                'forwardingScore': 0x1,
+                'isForwarded': !![],
+                'forwardedNewsletterMessageInfo': {
+                    'newsletterJid': _0x26c06f(0x204),
+                    'newsletterName': _0x26c06f(0x1fe),
+                    'serverMessageId': 0x3e7
+                },
+                'externalAdReply': {
+                    'title': _0x26c06f(0x1f0),
+                    'body': _0x26c06f(0x20f),
+                    'mediaType': 0x1,
+                    'sourceUrl': 'https://github.com/sadiyamin',
+                    'thumbnailUrl': _0x26c06f(0x201),
+                    'renderLargerThumbnail': ![],
+                    'showAdAttribution': !![]
+                }
+            }
+        }, {
+            'quoted': _0x51bfd4
+        }));
+    } catch (_0x22a8c7) {
+        console[_0x26c06f(0x207)](_0x22a8c7), _0x4be882(_0x26c06f(0x210));
+    }
+});
